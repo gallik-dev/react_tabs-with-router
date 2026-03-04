1ffe198423311b93be2205e16232e2b82be645bc
Maksym Halushko <gallikillag@gmail.com>
2026-03-04 18:48:41 +0200

    fix solution

--- src/Root.tsx
+++ src/Root.tsx
@@ -13,7 +13,7 @@ export const Root = () => (
     <Routes>
       <Route path="/" element={<App />}>
         <Route index element={<HomePage />} />
-        <Route path="/home" element={<Navigate to="/" replace />} />
+        <Route path="home" element={<Navigate to="/" replace />} />
 
         <Route path="tabs">
           <Route index element={<TabsPage />} />
